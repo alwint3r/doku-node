4ef4d9bfda011df1beee759f782f12f82f41dbd7
Alwin Arrasyid <alwin.ridd@gmail.com>
2016-07-28 21:49:13 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function Factory(config) {
+    var context = config;
+
+    if (!config.mallId) {
+        throw new Error('mallId must be provided!');
+    }
+
+    if (!config.sharedKey) {
+        throw new Error('sharedKey must be provided!');
+    }
+
+    context.baseUrl = config.isProduction
+        ? 'https://pay.doku.com/api'
+        : 'http://staging.doku.com/api';
+
+    return {
+        words: require('./src/words')(context),
+    };
+};
---
+++ src/words.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var crypto = require('crypto');
+
+function preGenerate(context, data) {
+    return [
+        data.amount,
+        context.mallId,
+        context.sharedkey,
+        data.invoice,
+    ];
+}
+
+function generateRaw(context, data) {
+    if (data.device_id) {
+        if (data.pairing_code) {
+            return preGenerate(context, data).concat([
+                data.currency,
+                data.token,
+                data.pairing_code,
+                data.device_id,
+            ]).join('');
+        } else {
+            return preGenerate(context, data).concat([
+                data.currency,
+                data.device_id,
+            ]).join('');
+        }
+    } else if (data.pairing_code) {
+        return preGenerate(context, data).concat([
+            data.currency,
+            data.token,
+            data.pairing_code,
+        ]).join('');
+    } else if (data.currency) {
+        return preGenerate(context, data).concat([
+            data.currency,
+        ]).join('');
+    } else {
+        return preGenerate(context, data).join('');
+    }
+}
+
+function makeSha1Hash(data) {
+    return crypto
+        .createHash('sha1')
+        .update(data)
+        .digest('hex');
+}
+
+module.exports = function(context) {
+    return {
+        raw: function raw(data) {
+            return generateRaw(context, data);
+        },
+
+        hashed: function hashed(data) {
+            return makeSha1Hash(generateRaw(context, data));
+        },
+    };
+};
